ac7fbb8c3e885d5104769c78d76313fb3dfe387b
Van Daele D <dandonio@hotmail.com>
2013-07-08 00:29:57 +0200

    deleting duplicate files

--- is_ip_address_up.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-ip_address=$1;
-ping -c 1 -W 1 $ip_address>/dev/null; 
-if [ $? -eq 0 ]; then 
-    echo "ip_address(up, '$ip_address')."; 
-else 
-    echo "ip_address(down, '$ip_address')."; 
-fi ; 
