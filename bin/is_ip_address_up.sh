7d95e3df321655cf15b728cfb30cb127869f81e6
Van Daele D <dandonio@hotmail.com>
2013-07-08 00:28:21 +0200

    added some hierarchy

---
+++ bin/is_ip_address_up.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+ip_address=$1;
+ping -c 1 -W 1 $ip_address>/dev/null; 
+if [ $? -eq 0 ]; then 
+    echo "ip_address(up, '$ip_address')."; 
+else 
+    echo "ip_address(down, '$ip_address')."; 
+fi ; 
