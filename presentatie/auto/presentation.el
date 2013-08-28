7f1ddc3976771e5614a2fc11db960022b895cf6a
Van Daele D <dandonio@hotmail.com>
2013-08-28 23:34:40 +0200

    presentatie update

--- presentatie/auto/presentation.el
+++ presentatie/auto/presentation.el
@@ -1,13 +1,25 @@
 (TeX-add-style-hook "presentation"
  (lambda ()
     (TeX-add-symbols
-     '("LINEIF" 2))
+     '("LINEIF" 2)
+     "headcol"
+     "rowcol"
+     "topline"
+     "midline"
+     "rowmidlinecw"
+     "rowmidlinewc"
+     "rowmidlinew"
+     "rowmidlinec"
+     "bottomline"
+     "bottomlinec")
     (TeX-run-style-hooks
      "wrapfig"
      "fancybox"
      "algorithmic"
      "noend"
      "adjustbox"
+     "booktabs"
+     "colortbl"
      "algorithm"
      "latex2e"
      "beamer10"
